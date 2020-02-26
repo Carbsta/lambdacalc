5155fc7310bfa08ca0d6b0153d2eda4be43d2397
Carbsta <totallycarbon@gmail.com>
2020-02-26 18:16:24 +0000

    starting work on UI for term construction

--- Main.hs
+++ Main.hs
@@ -5,6 +5,47 @@ import           ChurchEncodings
 import           Evaluator
 import           Parser
 import           Text.Parsec
+import qualified Graphics.UI.Threepenny            as UI
+import           Graphics.UI.Threepenny.Core
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    startGUI defaultConfig
+        { jsPort       = Just 8023
+        , jsStatic     = Just ""
+        } setup
+
+canWidth,canHeight :: Num a => a
+canWidth  = 300
+canHeight = 300
+
+mkCanvas :: Int -> Int -> UI Element
+mkCanvas h w = UI.canvas # set UI.height h
+      # set UI.width w
+      # set style [("border", "solid black 1px"), ("background", "#eee")]
+
+mkToolbar :: UI Element
+mkToolbar = UI.div #. "toolbar"
+
+drawWire :: UI.Canvas -> [UI.Point] -> UI ()
+drawWire c (p:ps) = do
+    UI.beginPath c
+    UI.moveTo p c
+    mapM_ (\x -> UI.lineTo x c) ps
+    UI.stroke c
+
+testPoints :: [UI.Point]
+testPoints = [(50,50),(50,100),(100,100)]
+
+setup :: Window -> UI ()
+setup window = do
+   return window # set UI.title "Term Builder"
+   toolbar <- mkToolbar
+   canvas <- mkCanvas canWidth canHeight # set UI.droppable True
+   --UI.fillRect (50.0,50.0) 50.0 50.0 canvas
+   drawWire canvas testPoints
+   UI.addStyleSheet window "style.css"
+   UI.addStyleSheet window "widgets.css"
+   -- b <- UI.div #. "drag-me" #  set text "Drag me around!" # set UI.draggable True # set UI.dragData "dragging"
+   getBody window #+ [column [pure canvas]]
+   return ()
