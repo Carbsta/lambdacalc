48b147b20b83b3cec188513bc07e637f4c284dd1
Carbsta <totallycarbon@gmail.com>
2020-03-18 17:08:51 +0000

    Built in standard definitions

--- Parser.hs
+++ Parser.hs
@@ -21,10 +21,29 @@ lapp :: Parser LTerm
 lapp = do {ts <- many1 unit; return (foldl1 LApp ts)}
 
 unit :: Parser LTerm
-unit = lvar +++ paren
+unit = lvar +++ paren +++ definition
 
 lvar :: Parser LTerm
 lvar = do {v <- token letter; return (LVar [v])}
 
 paren :: Parser LTerm
 paren = do {token $ char '('; term <- lterm; token $ char ')'; return term}
+
+definition :: Parser LTerm
+definition = do char '$'
+                name <- token $ many1 letter
+                return $ fst $ head $ parseTerm $ case name of
+                    "fix" -> "λf.(λx.f (x x))(λx.f(x x))"
+                    "omega" -> "(λx.x x)(λy.y y)"
+                    "times" -> "λm.λn.m($plus n) $zero"
+                    "plus" -> "λm.λn.λs.λz.m s(n s z)"
+                    "succ" -> "λn.λs.λz.s(n s z)"
+                    "zero" -> "λs.λz.z"
+                    "snd" -> "λp.p $false"
+                    "fst" -> "λp.p $true"
+                    "pair" -> "λf.λs.λb.b f s"
+                    "if" -> "λb.λt.λf.b t f"
+                    "false" -> "λt.λf.f"
+                    "true" -> "λt.λf.t"
+                    "id" -> "λx.x"
+                    _ -> error "Not in standard Library"
